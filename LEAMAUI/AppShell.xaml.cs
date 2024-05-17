7dbecbbc42eb7d92931f255c632575b76456bf73
AdrianWNoren <adrian.w.noren@gmail.com>
2024-05-17 08:57:08 +0200

    First commit added a default MAUI-project

---
+++ LEAMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LEAMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LEAMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LEAMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
